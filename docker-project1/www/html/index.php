9bbe1631c03a5e9fd59330ee27f92cd385e3c67a
khadijahassen <110534267+khadijahassen@users.noreply.github.com>
2022-08-04 13:49:35 +0200

    Add files via upload

---
+++ docker-project1/www/html/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>  
+     <head>  
+      <title>Solixy</title>
+     </head>   
+
+     <body>  
+      <h1> Solixy</h1>  
+      <p><?php echo 'solixy '  ?></p>  
+      <?   
+       $database ="mydbc";  
+       $user = "root";  
+       $password = "sect";  
+       $host = "mysql1";  
+
+       
+        ?>
+    </body>
+</html>
+
